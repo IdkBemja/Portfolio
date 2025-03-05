6b6936a4dcb2777ea60ec9c6f1af612afa4428c2
IdkBemja <idkorangedbenja72@gmail.com>
2025-03-04 23:12:38 -0300

    Update redirects.js

--- assets/js/redirects.js
+++ assets/js/redirects.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
     // Certificados href //
     $(document).on('click', '#iot-certification', function() {
-        window.open("", "_blank");
+        window.open("https://github.com/IdkBemja/Portfolio/raw/main/assets/documents/Introduction_to_IoT_certificate_benjaminmoraurra.pdf", "_blank");
     });
 
     $(document).on('click', '#cybersecurity-certification', function() {
-        window.open("", "_blank");
+        window.open("https://github.com/IdkBemja/Portfolio/raw/main/assets/documents/Cybersecurity_Essentials_certificate_benjaminmoraurra.pdf", "_blank");
     });
 
     $(document).on('click', '#fullstackpython-certification', function() {
-        window.open("", "_blank");
+        window.open("https://github.com/IdkBemja/Portfolio/raw/main/assets/documents/fullstackpython-bootcamp-benjaminmora.pdf", "_blank");
     });
 
     // Contacts and Project home btns href //
